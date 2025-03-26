4c75b1486090fad233834c24f50988dacc2bed0a
hakanyalniz <hakan_yalniz@live.com>
2025-03-26 21:11:02 +0300

    Completed exercise 1.7.

--- part1/unicafe/src/components/Statistics/Statistics.jsx
+++ part1/unicafe/src/components/Statistics/Statistics.jsx
@@ -1,10 +1,17 @@
 const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / total;
+  const positive = (good / total) * 100;
+
   return (
     <div>
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   );
 };
