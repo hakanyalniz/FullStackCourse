45aa05f35fe30aafa0555ae34a76ac89136b79d9
hakanyalniz <hakan_yalniz@live.com>
2025-03-27 21:13:18 +0300

    Completed exercise 2.9.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterWord, setFilterWord] = useState("");
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -32,9 +36,25 @@ const App = () => {
     }
   };
 
+  const handleFilterChange = (event) => {
+    setFilterWord(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((dict) =>
+    Object.values(dict).some(
+      (value) =>
+        typeof value === "string" && value.toLowerCase().includes(filterWord)
+    )
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <p>Filter shown with: </p>
+      <input value={filterWord} onChange={handleFilterChange} />
+
+      <h2>Add new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           <div>
@@ -49,13 +69,15 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {filteredPersons.map((person) => {
         return (
           <div key={person.name}>
             {person.name} {person.number}
           </div>
         );
       })}
+
+      {persons.filter((person) => Object.values(person).includes(filterWord))}
     </div>
   );
 };
