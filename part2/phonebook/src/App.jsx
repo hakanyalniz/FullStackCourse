dd4ecece1f72aad82d78d82b87024dcb6e83394a
hakanyalniz <hakan_yalniz@live.com>
2025-03-28 19:05:30 +0300

    Completed exercise 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Filter from "./components/Filter/Filter";
 import AddPerson from "./components/AddPerson/AddPerson";
 import Numbers from "./components/Numbers/Numbers";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [filterWord, setFilterWord] = useState("");
 
@@ -17,12 +14,23 @@ const App = () => {
     setFilterWord(event.target.value);
   };
 
+  // Go through persons array
+  // check each dictionaries value
+  // see if it is a string and if so see if it matches filterWord
+  // return true if so
+  // the dictionary we returned true on will become an item in new array
   const filteredPersons = persons.filter((dict) =>
     Object.values(dict).some(
       (value) =>
         typeof value === "string" && value.toLowerCase().includes(filterWord)
     )
   );
+
+  useEffect(() => {
+    axios.request("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
