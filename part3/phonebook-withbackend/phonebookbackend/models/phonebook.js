e94bf9001756cb8b80e501601e040d94e60042f1
hakanyalniz <hakan_yalniz@live.com>
2025-04-06 18:48:49 +0300

    Added some basic files for manipulating mongoose database.

--- part3/phonebook-withbackend/phonebookbackend/models/phonebook.js
+++ part3/phonebook-withbackend/phonebookbackend/models/phonebook.js
@@ -30,4 +30,4 @@ personSchema.set("toJSON", {
   },
 });
 
-module.exports = mongoose.model("Person", noteSchema);
+module.exports = mongoose.model("Person", personSchema);
--- part3/phonebook-withbackend/phonebookbackend/models/phonebookActions.js
+++ part3/phonebook-withbackend/phonebookbackend/models/phonebookActions.js
@@ -1,1 +1,29 @@
+// Actions used by the mongose database
 const mongoose = require("mongoose");
+const Phonebook = require("./phonebook");
+
+function addDB(body) {
+  const phonebook = new Phonebook({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  phonebook.save().then((result) => {
+    console.log("Person saved!");
+    return result;
+  });
+}
+
+async function deleteDB(id) {
+  const result = await phonebook.deleteOne({ _id: id });
+
+  if (result.deletedCount === 0) {
+    console.log("No document was deleted!");
+  }
+}
+
+async function findDB() {
+  return await phonebook.find({});
+}
+
+module.exports = { addDB, findDB, deleteDB };
