182d3b7c0979b76914cc51d989187e48c21547e6
hakanyalniz <hakan_yalniz@live.com>
2025-04-10 19:56:42 +0300

    Completed exercise 4.4.

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const result = blogs.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.likes,
+    0
+  );
+  return result;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
