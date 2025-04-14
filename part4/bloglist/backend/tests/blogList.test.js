71d1a8fbcfad407165427f58a5c9ae0613172752
hakanyalniz <hakan_yalniz@live.com>
2025-04-14 20:34:19 +0300

    Adding tests for upcoming exercise.

---
+++ part4/bloglist/backend/tests/blogList.test.js
@@ -0,0 +1,48 @@
+const { test, describe, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
+
+const mongoose = require("mongoose");
+const Blog = require("../models/blog");
+
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+
+const initialBlog = [
+  {
+    title: "How to tame your dragon",
+    author: "Taylor",
+    url: "www.tamingDragons.com/post01",
+    likes: 1002,
+  },
+  {
+    title: "The chilling tale of snowman",
+    author: "Drake",
+    url: "www.allthingssnow.com/snowman/01/chill",
+    likes: 2,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlog[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlog[1]);
+  await blogObject.save();
+});
+
+describe("HTTP GET request", () => {
+  test("verifies JSON format", async () => {
+    await api.get("/api/notes").expect("Content-Type", /application\/json/);
+  });
+
+  test("verifies blog post amount", async () => {
+    const response = await api.get("/api/notes");
+
+    assert.strictEqual(response.body.length, 2);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
