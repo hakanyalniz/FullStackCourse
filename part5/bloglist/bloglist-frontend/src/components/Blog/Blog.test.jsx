c78321e1f4e7a88788b6e55480e4ea426cf6c1d4
hakanyalniz <hakan_yalniz@live.com>
2025-05-05 19:22:29 +0300

    Completed exercise 5.14.

--- part5/bloglist/bloglist-frontend/src/components/Blog/Blog.test.jsx
+++ part5/bloglist/bloglist-frontend/src/components/Blog/Blog.test.jsx
@@ -1,35 +1,58 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
+import userEvent from "@testing-library/user-event";
+import { test } from "vitest";
 
-test("renders content", () => {
+const user = {
+  id: "user_id_s89dj19jdsdsadasdasd",
+  name: "TestName",
+  username: "TestUserName",
+  token: "test_token",
+};
+
+const blog = {
+  title: "Component testing is done with react-testing-library",
+  author: "TestUser",
+  url: "www.thisIsATest.com",
+  likes: 0,
+  id: "blog_id_6802919b710aed2rdr3",
+  user: user,
+};
+
+test("renders title and author for blog", () => {
   const mockSetStatus = vi.fn();
-
-  const user = {
-    id: "user_id_s89dj19jdsdsadasdasd",
-    name: "TestName",
-    username: "TestUserName",
-    token: "test_token",
-  };
-
-  const blog = {
-    title: "Component testing is done with react-testing-library",
-    author: "TestUser",
-    url: "www.thisIsATest.com",
-    likes: 0,
-    id: "blog_id_6802919b710aed2rdr3",
-    user: user,
-  };
 
   render(<Blog blog={blog} setBlogs={mockSetStatus} user={user} />);
 
   const elementTitle = screen.getByText(
     "Component testing is done with react-testing-library"
   );
-
   const elementAuthor = screen.getByText("TestUser");
-
-  screen.debug();
 
   expect(elementTitle).toBeDefined();
   expect(elementAuthor).toBeDefined();
+});
+
+test("content other than title and author is not visible by default", () => {
+  const mockSetStatus = vi.fn();
+
+  render(<Blog blog={blog} setBlogs={mockSetStatus} user={user} />);
+
+  const elementURL = screen.getByText("www.thisIsATest.com");
+
+  expect(elementURL).not.toBeVisible();
+});
+
+test("content becomes visible after clicking view button", async () => {
+  const mockSetStatus = vi.fn();
+
+  render(<Blog blog={blog} setBlogs={mockSetStatus} user={user} />);
+
+  const actionUser = userEvent.setup();
+  const button = screen.getByText("View");
+  await actionUser.click(button);
+
+  const elementURL = screen.getByText("www.thisIsATest.com");
+
+  expect(elementURL).toBeVisible();
 });
