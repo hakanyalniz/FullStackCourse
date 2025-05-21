8bac3042d4e8e902cc9ed22f509d7f50f3402382
hakanyalniz <hakan_yalniz@live.com>
2025-05-21 19:56:15 +0300

    The filter component is now connected to the filter reducer.

--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,7 +1,14 @@
+import { createAction } from "../reducers/anecdoteFilterReducer";
+import { useSelector, useDispatch } from "react-redux";
+
 const Filter = () => {
+  const dispatch = useDispatch();
+
   const handleChange = (event) => {
     // input-field value is in variable event.target.value
     console.log(event.target.value);
+    const filter = event.target.value;
+    dispatch(createAction("FILTER", { filter }));
   };
   const style = {
     marginBottom: 10,
