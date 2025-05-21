effb8da0cf21513a85fcc0cb43a5b67da51885f3
hakanyalniz <hakan_yalniz@live.com>
2025-05-21 19:25:03 +0300

    Created a filter component file for filtering anecdotes.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { createAction } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
+import Filter from "./components/Filter";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
@@ -24,7 +25,10 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
+      <br />
       <AnecdoteList anecdotes={anecdotes} vote={vote} />
+      <br />
       <AnecdoteForm createAnecdote={createAnecdote} />
     </div>
   );
