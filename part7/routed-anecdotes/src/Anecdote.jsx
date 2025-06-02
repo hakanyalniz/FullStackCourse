d02cc8b7f747fbfaf5282b809e23df3d3a75ab22
hakanyalniz <hakan_yalniz@live.com>
2025-06-02 18:41:46 +0300

    Fixed bug in which anecdote would give error when trying to render an anecdote that did not exist.

--- part7/routed-anecdotes/src/Anecdote.jsx
+++ part7/routed-anecdotes/src/Anecdote.jsx
@@ -1,5 +1,11 @@
 const Anecdote = ({ anecdote }) => {
-  return <div>{anecdote.content}</div>;
+  // If anecdote is undefined, then it is true with !, in that case return anecdote not found
+  // otherwise render it
+  return !anecdote ? (
+    <div>404 - Anecdote not found</div>
+  ) : (
+    <div>{anecdote.content}</div>
+  );
 };
 
 export default Anecdote;
