4f4e55016ad5d88f75a206dc7ccf0571ae3c56e3
hakanyalniz <hakan_yalniz@live.com>
2025-06-23 16:49:02 +0300

    Setting up the backend to work with mongoDB.

---
+++ part8/library/models/authors-schema.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const uniqueValidator = require("mongoose-unique-validator");
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+  },
+  born: {
+    type: Number,
+  },
+});
+
+schema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Author", schema);
+
+export let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+    bookCount: 2,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+    bookCount: 1,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+    bookCount: 2,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    bookCount: 1,
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    bookCount: 1,
+  },
+];
