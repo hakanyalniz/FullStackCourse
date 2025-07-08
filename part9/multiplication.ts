d72038ce35c95871325ab81b403001ae338d5a4b
hakanyalniz <hakan_yalniz@live.com>
2025-07-08 17:44:20 +0300

    Accepting numbers via process.argv and used type library for types.

--- part9/multiplication.ts
+++ part9/multiplication.ts
@@ -2,7 +2,10 @@ const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 };
 
-multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is:");
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+
+multiplicator(a, b, `Multiplied numbers ${a} and ${b}, the result is:`);
 
 // Results in an error because does not match the type
 
